2f40f6c3c79b5dc61a7282ae4f8355b6a64a1298
Martin, Corinna <corinnamartin@my.unt.edu>
2024-03-03 21:42:09 +0000

    Update main.cpp to set up time step loop

--- main.cpp
+++ main.cpp
@@ -23,6 +23,18 @@ int main(){
         cout << it.Getx() << ", " << it.Gety() << endl;
     }*/
     
+    char type; //Character representing zone type
+
+    for(int i = 0; i < timeLimit; ++i){
+
+        for(auto it : mainList){
+
+            type = it.GetZoneType();
+            it.CheckAdjZones(type); //Analyze zone
+        }
+
+        //Print output for current time step if appropriate for specified refresh rate
+    } //Analyze every zone during each time step until time limit is reached
 
     return 0;
 }
