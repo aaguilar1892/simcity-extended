24a2d2c98359497ca559fafe21bb9d545e1a4b6a
Martin, Corinna <corinnamartin@my.unt.edu>
2024-03-03 21:36:18 +0000

    Update zone.cpp to add set and gets for population, workers, and goods

--- zone.cpp
+++ zone.cpp
@@ -1,5 +1,6 @@
 #include "zone.h"
 
+//Set and gets for zone coordinates and type
 void Zone::Setx(int xCoord){
 	x = xCoord;
 }
@@ -22,4 +23,34 @@ int Zone::Gety(){
 
 char Zone::GetZoneType(){
 	return zoneType;
+}
+
+//Set and gets for zone population, workers, goods
+void Zone::SetPop(int pop){
+	numPop = pop;
+}
+
+void Zone::SetWorkers(int workers){
+	numWorkers = workers;
+}
+
+void Zone::SetGoods(int goods){
+	numGoods = goods;
+}
+
+int Zone::GetPop(){
+	return numPop;
+}
+
+int Zone::GetWorkers(){
+	return numWorkers;
+}
+
+int Zone::GetGoods(){
+	return numGoods;
+}
+
+//Function for analyzing zone
+void Zone::CheckAdjZones(char type){
+	
 }
