9997c7ca361feceee5dccaa5a1691132a03ad2ed
Womble, Britton <brittonwomble@my.unt.edu>
2024-03-04 14:46:32 +0000

    Update output.cpp (adding print based on coordinates)

--- output.cpp
+++ output.cpp
@@ -35,3 +35,62 @@ void printOutput(list<Zone> &mainList){
         cout<<endl;
     }
 }
+
+void printArea(list<Zone> &mainList){
+    //initializing coordinatesm for area
+    int xcoord1=0;
+    int ycoord1=0;
+    int xcoord2=0;
+    int ycoord2=0;
+    
+   
+    //get row and col
+    int row=0;
+    int col=0;
+    for( auto it : mainList){
+        if(it.Getx() > col){
+            col= it.Getx();
+        }
+        if(it.Gety() > row){
+            row= it.Gety();
+        }
+    }
+    //create graph
+    string graph[row+1][col+1];
+    //add correct zone type to each corrdinate
+    for(auto it = mainList.begin(); it != mainList.end(); ++it){
+            graph[it->Gety()][it->Getx()]= it->GetZoneType();
+    }
+    for(int i=0; i<5; ++i){
+        //cout the area for input from user
+        cout<<"What rectangular area would you like printed? "<<endl;
+        cout<<"Please enter the x-coordinate for top left corner of area: ";
+        cin>>xcoord1;
+        cout<<"Please enter the y-coordinate for top left corner of area: ";
+        cin>>ycoord1;
+        //cout<<endl;
+        cout<<"Please enter the x-coordinate for bottom right corner of area: ";
+        cin>>xcoord2;
+        cout<<"Please enter the y-coordinate for bottom right corner of area: ";
+        cin>>ycoord2;
+        
+        if(xcoord1>row || ycoord1>col || xcoord2> row|| ycoord2>col){
+            cout<<"Coordinates not valid, please try again"<<endl;
+        }else{
+            break;
+        }
+    }
+    
+    for(int i= ycoord1; i<=ycoord2; ++i){
+        for(int j=xcoord1; j<= xcoord2; ++j){
+            if(graph[i][j] =="R" || graph[i][j] == "I" || graph[i][j] == "C" || graph[i][j] == "T" || graph[i][j] == "-" || graph[i][j] == "#" || graph[i][j] == "P" || graph[i][j] == "1" || graph [i][j] == "2" || graph[i][j] == "3"){
+                cout<<graph[i][j]<<"  ";
+            }
+            else{
+                cout<<"   ";
+            }
+        }
+        cout<<endl;
+    }
+}
+
