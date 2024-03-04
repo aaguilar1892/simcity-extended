fe620c71b84fe3771db629987d5bc34c465e85a3
Womble, Britton <brittonwomble@my.unt.edu>
2024-03-04 13:02:26 +0000

    Update output.cpp (converted graph to string so it can output numbers and letters)

--- output.cpp
+++ output.cpp
@@ -16,15 +16,16 @@ void printOutput(list<Zone> &mainList){
     //cout<<row<<endl;
     //cout<<col<<endl;
     //create graph
-    char graph[row+1][col+1];
+     string graph[row+1][col+1];
     //add correct zone type to each corrdinate
     for(auto it = mainList.begin(); it != mainList.end(); ++it){
             graph[it->Gety()][it->Getx()]= it->GetZoneType();
     }
     //print the graph
+
     for(int i= 0; i<= row; ++i){
         for(int j= 0; j<= col; ++j){
-            if(graph[i][j] =='R' || graph[i][j] == 'I' || graph[i][j] == 'C' || graph[i][j] == 'T' || graph[i][j] == '-' || graph[i][j] == '#' || graph[i][j] == 'P'){
+            if(graph[i][j] =="R" || graph[i][j] == "I" || graph[i][j] == "C" || graph[i][j] == "T" || graph[i][j] == "-" || graph[i][j] == "#" || graph[i][j] == "P" || graph[i][j] == "1" || graph [i][j] == "2" || graph[i][j] == "3"){
                 cout<<graph[i][j]<<"  ";
             }
             else{
