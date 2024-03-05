6bb36ec42264dc0785bd18b4feda86868e928229
Womble, Britton <brittonwomble@my.unt.edu>
2024-03-05 01:02:02 +0000

    Update WorkersAndGoods.h

--- WorkersAndGoods.h
+++ WorkersAndGoods.h
@@ -3,4 +3,4 @@
 using namespace std;
 
 void SetWorkers(bool inc, int &workers);
-void SetGoods(bool inc, int &goods); //Functions for setting available workers & goods
+void SetGoods(bool inc, int &goods); //Functions for setting available workers & good
