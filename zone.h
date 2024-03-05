e3c3184edcd557c5cb782521907bebbfda0ceecf
Womble, Britton <brittonwomble@my.unt.edu>
2024-03-05 00:32:26 +0000

    Update zone.h

--- zone.h
+++ zone.h
@@ -1,35 +1,33 @@
 #include <iostream>
 #include <list>
+#include <string>
 #include "WorkersAndGoods.h"
 
 using namespace std;
 class Zone{
 
-	private:
+    private:
 
-		int x;
-		int y; //x & y coordinates of zone
+        int x;
+        int y; //x & y coordinates of zone
 
-		char zoneType;
-		int numPop; //Variables for zone population & type
-
+        char zoneType;
+        int numPop; //Variables for zone population & type
 		//Add list variable for adjacent zones
 		list<Zone> adjZone;
 
-	public:
+    public:
 
-		void Setx(int xCoord);
-		void Sety(int yCoord);
-		void SetZoneType(char type);
-		int Getx();
-		int Gety();
-		char GetZoneType(); //Get & sets for zone coordinates and type
+        void Setx(int xCoord);
+        void Sety(int yCoord);
+        void SetZoneType(char type);
+        int Getx();
+        int Gety();
+        char GetZoneType(); //Get & sets for zone coordinates and type
+        int GetPop(); //Get zone population
+        void SetPop(int pop);
 
-		int GetPop(); //Get zone population
-		//void SetPop(int pop);
+       void CheckAdjZones(int workers, int goods, int changed);
 
-		void CheckAdjZones(int workers, int goods, int changed);
-
-		//Add default constructor for zone w/population 0
+        //Add default constructor for zone w/population 0
 };
-
