e5f39419de7f95313e758b0b7ef43afadd139cfe
Martin, Corinna <corinnamartin@my.unt.edu>
2024-03-05 20:27:02 +0000

    Update zone.h to add function declarations for default Zone constructor and CheckAdjZones functions

--- zone.h
+++ zone.h
@@ -14,20 +14,24 @@ class Zone{
         char zoneType;
         int numPop; //Variables for zone population & type
 		//Add list variable for adjacent zones
-		list<Zone> adjZone;
+		list<Zone> adjZones;
 
     public:
 
         void Setx(int xCoord);
         void Sety(int yCoord);
         void SetZoneType(char type);
+        void SetPop(int pop);
+        void SetAdjList(Zone adjZone);
+
         int Getx();
         int Gety();
         char GetZoneType(); //Get & sets for zone coordinates and type
         int GetPop(); //Get zone population
-        void SetPop(int pop);
+        
 
-       void CheckAdjZones(int workers, int goods, int changed);
+       void CheckAdjZones(int workers, int goods, int &changed);
+       Zone();
 
         //Add default constructor for zone w/population 0
 };
