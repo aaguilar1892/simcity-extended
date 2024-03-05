01fd60ccb6c0b98fc1a09ffb9f1e13d75ae27df1
Womble, Britton <brittonwomble@my.unt.edu>
2024-03-05 01:03:11 +0000

    Update initialization.cpp

--- initialization.cpp
+++ initialization.cpp
@@ -29,36 +29,37 @@ void InitializeSim(list<Zone> &mainList, int &timeLimit, int &refreshRate){
 
     fin.open(regionFile);
     if(fin.fail()){
-	cout << "File error." << endl;
+    cout << "File error." << endl;
         exit(1);
     } //Check if region file opens correctly
 
+    //Until file end
     int xCount = 0;
-    int yCount = 0; //For setting the x & y coords of the zones
+    int yCount = 0;
+    int pop= 0;
 
     char myZone;
     Zone currZone;
 
     while(!fin.eof()){
-	    myZone = fin.get();
-	    
-	    if(myZone == 'R' || myZone == 'I' || myZone == 'C' || myZone == 'T' || myZone == '-' || myZone == '#' || myZone == 'P' || myZone == ' '){
-	    	
-		    currZone.SetZoneType(myZone);
-		    currZone.Setx(xCount);
-		    currZone.Sety(yCount);
-		    mainList.push_back(currZone);
+        myZone = fin.get();
+        
+        if(myZone == 'R' || myZone == 'I' || myZone == 'C' || myZone == 'T' || myZone == '-' || myZone == '#' || myZone == 'P'){
+            
+            currZone.SetZoneType(myZone);
+            currZone.Setx(xCount);
+            currZone.Sety(yCount);
+            currZone.SetPop(pop);
+            mainList.push_back(currZone);
 
-	    } //Assign character to represent zone type & assign x,y coords of zone, then add to the list of zones
+        } //Assign character to represent zone type & assign x,y coords of zone, then add to the list of zones
 
-	    if(myZone != '\n' && myZone != ',' /*&& myZone != ' '*/){
-		    ++xCount;
-	    }
-	    if(myZone == '\n'){
-		    ++yCount;
-		    xCount = 0;
-	    } //Increase x-coord by 1 until end of line, then reset and increase y-coord by 1
+        if(myZone != '\n' && myZone != ',' /*&& myZone != ' '*/){
+            ++xCount;
+        }
+        if(myZone == '\n'){
+            ++yCount;
+            xCount = 0;
+        } //Increase x-coord by 1 until end of line, then reset and increase y-coord by 1
     }
-
-    fin.close();
 }
