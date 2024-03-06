9b0b24a899182d41c120cc5917563efa69e8296e
Womble, Britton <brittonwomble@my.unt.edu>
2024-03-06 14:55:12 +0000

    Update main.cpp fixed

--- main.cpp
+++ main.cpp
@@ -3,42 +3,38 @@
 using namespace std;
 
 int main(){
-
+    
     int timeLimit;
     int refreshRate;
-
+    
     int workers = 0;
     int goods = 0; //Variables for # available workers & goods
-
+    
     list<Zone> mainList; //List of all zones
-    list<Zone> resident;
-    list<Zone> industrial;
-    list<Zone> commerical;
-
+    
     InitializeSim(mainList, timeLimit, refreshRate); //Initializes mainList, timeLimit, refreshRate, & zone coordinates
-    setZone(mainList, resident, industrial, commerical);
     
     //Test time limit & refresh rate assigned correctly
     /*
-    cout << "Time limit: " << timeLimit << endl;
-    cout << "Refresh rate: " << refreshRate << endl;
-    */
+     cout << "Time limit: " << timeLimit << endl;
+     cout << "Refresh rate: " << refreshRate << endl;
+     */
     //Test zone coordinates assigned correctly
-        
-    /*for(auto it : resident){
-
-        cout << it.Getx() << ", " << it.Gety() << endl;
-    }*/
     
+    /*for(auto it : resident){
+     
+     cout << it.Getx() << ", " << it.Gety() << endl;
+     }*/
     int changed=0;
-
+    
     for(int i = 0; i < timeLimit; ++i){
         changed=0;
         
-        for(auto it : mainList){
-            it.CheckAdjZones(workers, goods, changed, mainList); //Analyze zone
+        for(auto& it : mainList){
+            it.CheckAdjZones(workers, goods, changed, mainList);//Analyze zone
         }
         
+        //if there is no change inbetween it stops the loop
         if(changed == 0){
             break;
         }
@@ -47,20 +43,22 @@ int main(){
         if(i%refreshRate == 0){
             printOutput(mainList);
         }
-        cout<<"Time Step: "<<i+1<<endl;
-        cout<<"Available Workers: "<<workers<<endl;
-        cout<<"Available Goods: "<<goods<<endl;
 
-    } //Analyze every zone during each time step until time limit is reached
+    cout<<"Time Step: "<<i+1<<endl;
+    cout<<"Available Workers: "<<workers<<endl;
+    cout<<"Available Goods: "<<goods<<endl;
+}
+
+    //Analyze every zone during each time step until time limit is reached
+    
     
     //final print
     printOutput(mainList);
-    
     //print final population of each zone
-    printPop(resident, industrial, commerical);
+    printPop(mainList);
     
     //print area wanted by student and get coordinates
-    printArea(mainList,resident, industrial, commerical);
-    
+    printArea(mainList);
+
     return 0;
 }
