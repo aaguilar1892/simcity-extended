6b704ab7f86a5b2afdd4fa10d8f80aabce478848
Womble, Britton <brittonwomble@my.unt.edu>
2024-03-06 14:56:31 +0000

    Update output.h fixed

--- output.h
+++ output.h
@@ -4,11 +4,8 @@
 #include "initialization.h"
 using namespace std;
 
-
 void printOutput(list<Zone> &mainList);
 
-void printArea(list<Zone> &mainList, list<Zone> &resident,list<Zone> &industrial,list<Zone> &commerical);
+void printArea(list<Zone> &mainList);
 
-void setZone(list<Zone> &mainList, list<Zone> &resident,list<Zone> &industrial,list<Zone> &commerical);
-
-void printPop(list<Zone> &resident,list<Zone> &industrial,list<Zone> &commerical);
+void printPop(list<Zone> &mainList);
