d35a5065d7d985f2037a93c035c1abafa885d224
Womble, Britton <brittonwomble@my.unt.edu>
2024-03-06 14:56:01 +0000

    Update output.cpp fixed

--- output.cpp
+++ output.cpp
@@ -1,8 +1,8 @@
 #include "output.h"
 
 void printOutput(list<Zone> &mainList){
-    //getting cols and rows for graph
-   int row=0;
+    //get max row and col
+    int row=0;
     int col=0;
     for( auto it : mainList){
         if(it.Getx() > col){
@@ -17,7 +17,8 @@ void printOutput(list<Zone> &mainList){
     //cout<<col<<endl;
     //create graph
     string graph[row+1][col+1];
-    //add correct zone type to each corrdinate
+   
+   //get zonetype, if pop in that zone type is above zero then convert pop to string and put in graph in place of zone type
     for(auto it = mainList.begin(); it != mainList.end(); ++it){
         if(it->GetZoneType() == 'R'|| it->GetZoneType() == 'I' || it->GetZoneType() == 'C'){
             if(it->GetPop() > 0){
@@ -25,25 +26,25 @@ void printOutput(list<Zone> &mainList){
             }else{
                 graph[it->Gety()][it->Getx()]= it->GetZoneType();
             }}else{
-            graph[it->Gety()][it->Getx()]= it->GetZoneType();
-        }
-}
-        //print the graph
-        for(int i= 0; i<= row; ++i){
-            for(int j= 0; j<= col; ++j){
-                if(graph[i][j] =="R" || graph[i][j] == "I" || graph[i][j] == "C" || graph[i][j] == "T" || graph[i][j] == "-" || graph[i][j] == "#" || graph[i][j] == "P" || graph[i][j] == "1" || graph [i][j] == "2" || graph[i][j] == "3" || graph[i][j] == "4"){
-                    cout<<graph[i][j]<<"  ";
-                }
-                else{
-                    cout<<"   ";
-                }
+                graph[it->Gety()][it->Getx()]= it->GetZoneType();
             }
-            cout<<endl;
-        }
-    
+    }
+       //print the graph
+       for(int i= 0; i<= row; ++i){
+           for(int j= 0; j<= col; ++j){
+               if(graph[i][j] =="R" || graph[i][j] == "I" || graph[i][j] == "C" || graph[i][j] == "T" || graph[i][j] == "-" || graph[i][j] == "#" || graph[i][j] == "P" || graph[i][j] == "1" || graph [i][j] == "2" || graph[i][j] == "3" || graph[i][j] == "4" || graph[i][j] == "5" || graph[i][j] == "6" || graph[i][j] == "8" || graph[i][j] == "9" || graph[i][j] == "10"){
+                   cout<<graph[i][j]<<"  ";
+               }
+               else{
+                   cout<<"   ";
+               }
+           }
+           cout<<endl;
+       }
+
 }
 
-void printArea(list<Zone> &mainList, list<Zone> &resident,list<Zone> &industrial,list<Zone> &commerical){
+void printArea(list<Zone> &mainList){
     //initializing coordinatesm for area
     int xcoord1=0;
     int ycoord1=0;
@@ -62,7 +63,8 @@ void printArea(list<Zone> &mainList, list<Zone> &resident,list<Zone> &industrial
             row= it.Gety();
         }
     }
-  
+    
+  //ask user for coords
     for(int i=0; i<5; ++i){
         //cout the area for input from user
         cout<<"What rectangular area would you like printed? "<<endl;
@@ -85,22 +87,23 @@ void printArea(list<Zone> &mainList, list<Zone> &resident,list<Zone> &industrial
     
     //create graph
     string graph[row+1][col+1];
-    //add correct zone type to each corrdinate
- 
+    
+    //get zonetype, if pop in that zone type is above zero then convert pop to string and put in graph in place of zone type
     for(auto it = mainList.begin(); it != mainList.end(); ++it){
         if(it->GetZoneType() == 'R'|| it->GetZoneType() == 'I' || it->GetZoneType() == 'C'){
             if(it->GetPop() > 0){
-                graph[it->Gety()][it->Getx()]= it->GetPop();
+                graph[it->Gety()][it->Getx()]= to_string(it->GetPop());
             }else{
                 graph[it->Gety()][it->Getx()]= it->GetZoneType();
             }}else{
             graph[it->Gety()][it->Getx()]= it->GetZoneType();
             }
     }
-  
+    
+    //print graph
     for(int i= ycoord1; i<=ycoord2; ++i){
         for(int j=xcoord1; j<= xcoord2; ++j){
-            if(graph[i][j] =="R" || graph[i][j] == "I" || graph[i][j] == "C" || graph[i][j] == "T" || graph[i][j] == "-" || graph[i][j] == "#" || graph[i][j] == "P" || graph[i][j] == "1" || graph [i][j] == "2" || graph[i][j] == "3" || graph[i][j] == "4"){
+            if(graph[i][j] =="R" || graph[i][j] == "I" || graph[i][j] == "C" || graph[i][j] == "T" || graph[i][j] == "-" || graph[i][j] == "#" || graph[i][j] == "P" || graph[i][j] == "1" || graph [i][j] == "2" || graph[i][j] == "3" || graph[i][j] == "4"|| graph[i][j] == "5" || graph[i][j] == "6" || graph[i][j] == "8" || graph[i][j] == "9" || graph[i][j] == "10"){
                 cout<<graph[i][j]<<"  ";
             }
             else{
@@ -109,70 +112,31 @@ void printArea(list<Zone> &mainList, list<Zone> &resident,list<Zone> &industrial
         }
         cout<<endl;
     }
-    for(auto it = mainList.begin(); it != mainList.end(); ++it){
-        if(xcoord1<=row && ycoord1<=col && xcoord2<=row && ycoord2<=col){
-            Zone currZone;
-            for(auto it = mainList.begin(); it != mainList.end(); ++it){
-                if(it->GetZoneType() == 'R'){
-                    currZone.Setx(it->Getx());
-                    currZone.Sety(it->Gety());
-                    currZone.SetZoneType(it->GetZoneType());
-                    resident.push_back(currZone);
-                }else if(it->GetZoneType() == 'I'){
-                    currZone.Setx(it->Getx());
-                    currZone.Sety(it->Gety());
-                    currZone.SetZoneType(it->GetZoneType());
-                    industrial.push_back(currZone);
-                }else if(it->GetZoneType() == 'C'){
-                    currZone.Setx(it->Getx());
-                    currZone.Sety(it->Gety());
-                    currZone.SetZoneType(it->GetZoneType());
-                    commerical.push_back(currZone);
-                }
-            }
-        }
-    }
     
+    //initalize the pops and the bounds
     int resPop=0;
     int comPop=0;
     int indPop=0;
+    int x=0;
+    int y=0;
+            
+    //get pops inside of bounds from the main list and add to pops for later output
+    for(auto it = mainList.begin(); it != mainList.end(); ++it){
+        x= it->Getx();
+        y= it->Gety();
+        
+        if(x >= xcoord1 && x <= xcoord2 && y >= ycoord1 && y <= ycoord2){
+            if(it->GetZoneType() == 'R'&& it->GetPop()> 0){
+                resPop=resPop+it->GetPop();
+            }else if(it->GetZoneType() == 'C'&& it->GetPop()> 0){
+                comPop=comPop + it->GetPop();
+            }else if(it->GetZoneType() == 'I'&& it->GetPop()> 0){
+                indPop=indPop + it->GetPop();
+            }else{}
+        }
+    }
     
-    for(auto it = resident.begin(); it !=resident.end(); ++it){
-        if(it->GetPop() == 1){
-            resPop= resPop + 1;
-        }else if(it->GetPop() == 2){
-            resPop= resPop + 2;
-        }
-        else if(it->GetPop() == 3){
-            resPop= resPop + 3;
-        }else if(it->GetPop() == 4){
-            resPop= resPop + 4;
-        }
-    }
-    for(auto it = industrial.begin(); it !=industrial.end(); ++it){
-        if(it->GetPop() == 1){
-            indPop= indPop + 1;
-        }else if(it->GetPop() == 2){
-            indPop= indPop + 2;
-        }
-        else if(it->GetPop() == 3){
-            indPop= indPop + 3;
-        }else if(it->GetPop() == 4){
-            indPop= indPop + 4;
-        }
-    }
-    for(auto it = commerical.begin(); it !=commerical.end(); ++it){
-        if(it->GetPop() == 1){
-            comPop= comPop + 1;
-        }else if(it->GetPop() == 2){
-            comPop= comPop + 2;
-        }
-        else if(it->GetPop() == 3){
-            comPop= comPop + 3;
-        }else if(it->GetPop() == 4){
-            comPop= comPop + 4;
-        }
-    }
+    //print pops
     cout<<"Populations for specific area:"<<endl;
     cout<<"Residental Zone: "<<resPop<<endl;
     cout<<"Industrial Zone: "<<indPop<<endl;
@@ -180,73 +144,26 @@ void printArea(list<Zone> &mainList, list<Zone> &resident,list<Zone> &industrial
     
 }
 
-void setZone(list<Zone> &mainList, list<Zone> &resident,list<Zone> &industrial,list<Zone> &commerical){
-    
-    Zone currZone;
-    for(auto it = mainList.begin(); it != mainList.end(); ++it){
-        if(it->GetZoneType() == 'R'){
-            currZone.Setx(it->Getx());
-            currZone.Sety(it->Gety());
-            currZone.SetZoneType(it->GetZoneType());
-            resident.push_back(currZone);
-        }else if(it->GetZoneType() == 'I'){
-            currZone.Setx(it->Getx());
-            currZone.Sety(it->Gety());
-            currZone.SetZoneType(it->GetZoneType());
-            industrial.push_back(currZone);
-        }else if(it->GetZoneType() == 'C'){
-            currZone.Setx(it->Getx());
-            currZone.Sety(it->Gety());
-            currZone.SetZoneType(it->GetZoneType());
-            commerical.push_back(currZone);
-        }
-    }
-}
-
-void printPop(list<Zone> &resident,list<Zone> &industrial,list<Zone> &commerical){
+void printPop(list<Zone> &mainList){
+    //initalize pops
     int resPop=0;
     int comPop=0;
     int indPop=0;
     
-    for(auto it = resident.begin(); it !=resident.end(); ++it){
-        if(it->GetPop() == 1){
-            resPop= resPop + 1;
-        }else if(it->GetPop() == 2){
-            resPop= resPop + 2;
-        }
-        else if(it->GetPop() == 3){
-            resPop= resPop + 3;
-        }else if(it->GetPop() == 4){
-            resPop= resPop + 4;
-        }
+    //get pops for each zone type to add up by iterating through mainlist
+    for(auto it = mainList.begin(); it != mainList.end(); ++it){
+        if(it->GetZoneType() == 'R'&& it->GetPop()> 0){
+            resPop=resPop+it->GetPop();
+        }else if(it->GetZoneType() == 'C'&& it->GetPop()> 0){
+            comPop=comPop + it->GetPop();
+        }else if(it->GetZoneType() == 'I'&& it->GetPop()> 0){
+            indPop=indPop + it->GetPop();
+        }else{}
     }
-    for(auto it = industrial.begin(); it !=industrial.end(); ++it){
-        if(it->GetPop() == 1){
-            indPop= indPop + 1;
-        }else if(it->GetPop() == 2){
-            indPop= indPop + 2;
-        }
-        else if(it->GetPop() == 3){
-            indPop= indPop + 3;
-        }else if(it->GetPop() == 4){
-            indPop= indPop + 4;
-        }
-    }
-    for(auto it = commerical.begin(); it !=commerical.end(); ++it){
-        if(it->GetPop() == 1){
-            comPop= comPop + 1;
-        }else if(it->GetPop() == 2){
-            comPop= comPop + 2;
-        }
-        else if(it->GetPop() == 3){
-            comPop= comPop + 3;
-        }else if(it->GetPop() == 4){
-            comPop= comPop + 4;
-        }
-    }
-    cout<<"Populations:"<<endl;
+       
+    //print pops
+    cout<<"Populations for specific area:"<<endl;
     cout<<"Residental Zone: "<<resPop<<endl;
     cout<<"Industrial Zone: "<<indPop<<endl;
     cout<<"Commercial Zone: "<<comPop<<endl;
-    
 }
