0cbc253a58cf52724284e57f9b1b4cdc62b56d26
Martin, Corinna <corinnamartin@my.unt.edu>
2024-03-10 02:10:12 +0000

    Update initialization.cpp to add comments

--- initialization.cpp
+++ initialization.cpp
@@ -6,16 +6,19 @@ void InitializeSim(list<Zone> &mainList, int &timeLimit, int &refreshRate){
     fstream fin;
     string regionFile;
 
+    //Get configuration file name from user
     cout << "Input Configuration File: ";
     cin >> configFile;
     cin.ignore();
 
+    //Check configuration file opens correctly
     fin.open(configFile);
     if(fin.fail()){
         cout << "File error." << endl;
         exit(1);
-    } //Check configuration file opens correctly
+    } 
 
+    //Get configuration data from file
     fin.ignore(14, ':');
     getline(fin, regionFile);
     fin.ignore(11, ':');
@@ -23,43 +26,48 @@ void InitializeSim(list<Zone> &mainList, int &timeLimit, int &refreshRate){
     fin.ignore();
     fin.ignore(13, ':');
     fin >> refreshRate;
-    fin.ignore(); //Get configuration data from file
+    fin.ignore(); 
 
     fin.close();
 
+    //Check if region file opens correctly
     fin.open(regionFile);
     if(fin.fail()){
     cout << "File error." << endl;
         exit(1);
-    } //Check if region file opens correctly
+    } 
 
-    //Until file end
     int xCount = 0;
-    int yCount = 0;
+    int yCount = 0; //Variables representing x & y coordinates
     int pop= 0;
 
-    char myZone;
+    char myZone; //Character representing the zone type
     Zone currZone;
 
     while(!fin.eof()){
+        //Assign character for zone type
         myZone = fin.get();
         
         if(myZone == 'R' || myZone == 'I' || myZone == 'C' || myZone == 'T' || myZone == '-' || myZone == '#' || myZone == 'P'){
             
             currZone.SetZoneType(myZone);
+
+            //Assign x & y coordinates of zone
             currZone.Setx(xCount);
             currZone.Sety(yCount);
             currZone.SetPop(pop);
+
+            //Add zone to the list of zones, mainList
             mainList.push_back(currZone);
+        }
 
-        } //Assign character to represent zone type & assign x,y coords of zone, then add to the list of zones
-
-        if(myZone != '\n' && myZone != ',' /*&& myZone != ' '*/){
+        //Increase x-coordinate by 1 until end of line, then reset and increase y-coordinate by 1
+        if(myZone != '\n' && myZone != ','){
             ++xCount;
         }
         if(myZone == '\n'){
             ++yCount;
             xCount = 0;
-        } //Increase x-coord by 1 until end of line, then reset and increase y-coord by 1
+        }
     }
 }
