c33a57b45dd1e5cd672609a1721b5fbf8bcf0733
Martin, Corinna <corinnamartin@my.unt.edu>
2024-04-03 19:40:53 +0000

    Update initialization.h to pass xmax and ymax as arguments to InitializeSim function

--- initialization.h
+++ initialization.h
@@ -4,4 +4,4 @@
 #include "zone.h"
 using namespace std;
 
-void InitializeSim(list<Zone> &mainList, int &timeLimit, int &refreshRate); //Initializes mainList, time limit, refresh rate, and zone coordinates
+void InitializeSim(list<Zone> &mainList, int &timeLimit, int &refreshRate, int &xmax, int &ymax); //Initializes mainList, time limit, refresh rate, and zone coordinates
