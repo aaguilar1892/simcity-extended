47015ce047fb5ac58616875ea4fdb08298501795
Martin, Corinna <corinnamartin@my.unt.edu>
2024-04-03 20:39:25 +0000

    Replace mapGen.h

--- mapGen.h
+++ mapGen.h
@@ -1,3 +1,6 @@
 #include "output.h"
+#include <ctime>
+#include <cstdlib>
 
+void EnvironmentGen(list<Zone> &mainList); //Sets environment type
 void InitializeMap(list<Zone> &mainList, int &xmax, int &ymax); //Initializes surrounding environment
