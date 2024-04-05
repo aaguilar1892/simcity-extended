f768c76cc0f80eef1d283d01420522b05df1900a
Womble, Britton <brittonwomble@my.unt.edu>
2024-04-05 22:24:33 +0000

    Update initialization.h indef

--- initialization.h
+++ initialization.h
@@ -1,3 +1,5 @@
+#ifndef initialization_h
+#define initialization_h
 #include <iostream>
 #include <fstream>
 #include <list>
@@ -5,3 +7,4 @@
 using namespace std;
 
 void InitializeSim(list<Zone> &mainList, int &timeLimit, int &refreshRate, int &xmax, int &ymax); //Initializes mainList, time limit, refresh rate, and zone coordinates
+#endif
