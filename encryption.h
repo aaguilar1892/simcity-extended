4b652c79b1035944f4992b4fdfa53ca93cbf8271
Pacquing, Claire <clairepacquing@my.unt.edu>
2024-04-07 19:24:20 +0000

    Create encryption header file

---
+++ encryption.h
@@ -0,0 +1,11 @@
+#ifndef encryption_h
+#define encryption_h
+#include <iostream>
+#include <math.h>
+#include <list>
+#include <vector>
+using namespace std;
+
+void publicKey(vector<int> &encryptVals);
+
+#endif
