26c4eef56a4fe522658af6a15024f46c76ddc086
Martin, Corinna <corinnamartin@my.unt.edu>
2024-04-07 22:29:46 +0000

    Update build.h to add comments

--- build.h
+++ build.h
@@ -2,7 +2,7 @@
 #include "output.h"
 using namespace std;
 
-void BuildMenu(int totalPop, int &builtZones, list<Zone> &mainList);
-void Build(char choice, int totalPop, int &builtZones, list<Zone> &mainList, int xCoord, int yCoord);
-bool ZoneValidity(int xCoord, int yCoord, list<Zone> mainList);
-void ChangeZoneType(char newType, int xCoord, int yCoord, list<Zone> &mainList);
+void BuildMenu(int totalPop, int &builtZones, list<Zone> &mainList); //Outputs build menu
+void Build(char choice, int totalPop, int &builtZones, list<Zone> &mainList, int xCoord, int yCoord); //Builds new city zones
+bool ZoneValidity(int xCoord, int yCoord, list<Zone> mainList); //Checks if zone can be developed
+void ChangeZoneType(char newType, int xCoord, int yCoord, list<Zone> &mainList); //Changes environment zone to city zone
