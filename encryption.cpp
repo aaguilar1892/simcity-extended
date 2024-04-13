119c9579a35811097ad44512d9691018ec93f7da
Pacquing, Claire <clairepacquing@my.unt.edu>
2024-04-13 23:14:50 +0000

    Update encryption.cpp. Fixed public key.

--- encryption.cpp
+++ encryption.cpp
@@ -2,15 +2,18 @@
 
 //create encyption key using RSA
 void publicKey(vector<int> &encryptVals){
-    int userval;
-    int loop = 1;
+    int userval; 
+    int loop = 1; 
+    int n; 
+    int phi; 
+    int e;//variables for creating the public key
     
     //only need two values so it loops twice
     while(loop != 3){
         
         //enter a value greater than zero for algorithm and modulo
-        cout << "Please enter an integer value that is greater than 0." << endl; 
-        cout << "Please Enter Value: " << endl;
+        cout << "Please enter a prime integer value that is greater than 1." << endl; 
+        cout << "Please Enter Value: ";
         cin >> userval;
         
         if(userval > 0){
@@ -21,6 +24,37 @@ void publicKey(vector<int> &encryptVals){
         }
     
     }
+    //create n which is the second part of the encryption key
+    n = encryptVals[0] * encryptVals[1];
+    encryptVals.push_back(n);
+    
+    //helps find phi which is used to find the first part of the encryption key and decryption key
+    phi = (encryptVals[0] - 1) * (encryptVals[1] - 1); 
+    encryptVals.push_back(phi);
+
+    //checking for coprime numbers
+    for(int i = 2; i < n; i++){
+        
+        if(i % 2 != 0){
+            
+            if(phi % i != 0){
+
+                if(n % i != 0){
+
+                    e = i;
+                    break;
+
+                }
+
+            }
+
+        }
+        
+    }
+    encryptVals.push_back(e);
+
+    cout << "Public Key: (" << encryptVals[4] << ", " << encryptVals[2] << ") " << endl;
+
 }
 
 /*void encryptSim(list<Zone> &mainList, vector<int> encryptVals){
